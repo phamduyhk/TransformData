0d0068d53e91e3858c61cc3f6cad53efccf553a5
phamduyhk <phamduyhk@gmail.com>
2018-12-21 20:33:10 +0900

    make

--- make.py
+++ make.py
@@ -32,79 +32,28 @@ def get_list_of_category(data,category):
 
 #process
 
-
-
 stores = {'2:花堂店', '3:春江店', '1:大願寺店'}
 
 list = glob.glob("./input/*.xlsx")
 
-
-data_1_2018 = pd.DataFrame()
-data_1_2017 = pd.DataFrame()
-data_1_2016 = pd.DataFrame()
-data_1_2015 = pd.DataFrame()
-
-data_2_2018 = pd.DataFrame()
-data_2_2017 = pd.DataFrame()
-data_2_2016 = pd.DataFrame()
-data_2_2015 = pd.DataFrame()
-
-data_3_2018 = pd.DataFrame()
-data_3_2017 = pd.DataFrame()
-data_3_2016 = pd.DataFrame()
-data_3_2015 = pd.DataFrame()
-
-
-for item in list:
-    print("Preparing to read:"+str(item))
-    try:
-        data = pd.read_excel(item)
-        for index,row in data.iterrows():
-            year = row['計上日付'].date().year
-            if row["店舗"] == "1:大願寺店":
-                if year == 2018 :
-                    data_1_2018 = data_1_2018.append(row,ignore_index=True)
-                if year == 2017 :
-                    data_1_2017 = data_1_2017.append(row,ignore_index=True)
-                if year == 2016 :
-                    data_1_2016 = data_1_2016.append(row,ignore_index=True)
-                if year == 2015 :
-                    data_1_2015 = data_1_2015.append(row,ignore_index=True)
-            elif row["店舗"] == "2:花堂店":
-                if year == 2018 :
-                    data_2_2018 = data_2_2018.append(row,ignore_index=True)
-                if year == 2017 :
-                    data_2_2017 = data_2_2017.append(row,ignore_index=True)
-                if year == 2016 :
-                    data_2_2016 = data_2_2016.append(row,ignore_index=True)
-                if year == 2015 :
-                    data_2_2015 = data_2_2015.append(row,ignore_index=True)
-            elif row["店舗"] == "3:春江店":
-                if year == 2018 :
-                    data_3_2018 = data_3_2018.append(row,ignore_index=True)
-                if year == 2017 :
-                    data_3_2017 = data_3_2017.append(row,ignore_index=True)
-                if year == 2016 :
-                    data_3_2016 = data_3_2016.append(row,ignore_index=True)
-                if year == 2015 :
-                    data_3_2015 = data_3_2015.append(row,ignore_index=True)
-    except Exception as ex:
+#take columns values (header)
+try:
+    header = pd.read_excel(list[0]).columns.values
+except Exception as ex:
         print('can not read file {} {}'.format(item, ex))
-    break
 
 
-"""
-save to csv file
-
-
-"""
+#output folder declare
 output_path = "./output/"
 if os.path.exists(os.path.dirname(output_path)) is False:
     try:
         os.makedirs(os.path.dirname(output_path))
     except OSError as ex:  # Guard against race condition
         raise ValueError('Folder is not exist {}'.format(output_path))
-      
+
+
+#output filename declare
+
 output1 ='output/store1_2018.csv'
 output2 ='output/store1_2017.csv'
 output3 ='output/store1_2016.csv'
@@ -120,18 +69,77 @@ output32 ='output/store3_2017.csv'
 output33 ='output/store3_2016.csv'
 output34 ='output/store3_2015.csv'
 
+for item in list:
+    print("Reading:"+str(item))
+    try:
+        #create output dataframe
+        data_1_2018 = pd.DataFrame(columns=header)
+        data_1_2017 = pd.DataFrame(columns=header)
+        data_1_2016 = pd.DataFrame(columns=header)
+        data_1_2015 = pd.DataFrame(columns=header)
 
-data_1_2018.to_csv(output1)
-data_1_2017.to_csv(output2)
-data_1_2016.to_csv(output3)
-data_1_2015.to_csv(output4)
+        data_2_2018 = pd.DataFrame(columns=header)
+        data_2_2017 = pd.DataFrame(columns=header)
+        data_2_2016 = pd.DataFrame(columns=header)
+        data_2_2015 = pd.DataFrame(columns=header)
 
-data_2_2018.to_csv(output21)
-data_2_2017.to_csv(output22)
-data_2_2016.to_csv(output23)
-data_2_2015.to_csv(output24)
+        data_3_2018 = pd.DataFrame(columns=header)
+        data_3_2017 = pd.DataFrame(columns=header)
+        data_3_2016 = pd.DataFrame(columns=header)
+        data_3_2015 = pd.DataFrame(columns=header)
 
-data_3_2018.to_csv(output31)
-data_3_2017.to_csv(output32)
-data_3_2016.to_csv(output33)
-data_3_2015.to_csv(output34)
+        #read input dataframe
+        data = pd.read_excel(item)
+        for index,row in data.iterrows():
+            year = row['計上日付'].date().year
+            if row["店舗"] == "1:大願寺店":
+                if year == 2018 :
+                    data_1_2018 = data_1_2018.append(row,ignore_index=True)
+                elif year == 2017 :
+                    data_1_2017 = data_1_2017.append(row,ignore_index=True)
+                elif year == 2016 :
+                    data_1_2016 = data_1_2016.append(row,ignore_index=True)
+                elif year == 2015 :
+                    data_1_2015 = data_1_2015.append(row,ignore_index=True)
+            elif row["店舗"] == "2:花堂店":
+                if year == 2018 :
+                    data_2_2018 = data_2_2018.append(row,ignore_index=True)
+                elif year == 2017 :
+                    data_2_2017 = data_2_2017.append(row,ignore_index=True)
+                elif year == 2016 :
+                    data_2_2016 = data_2_2016.append(row,ignore_index=True)
+                elif year == 2015 :
+                    data_2_2015 = data_2_2015.append(row,ignore_index=True)
+            elif row["店舗"] == "3:春江店":
+                if year == 2018 :
+                    data_3_2018 = data_3_2018.append(row,ignore_index=True)
+                elif year == 2017 :
+                    data_3_2017 = data_3_2017.append(row,ignore_index=True)
+                elif year == 2016 :
+                    data_3_2016 = data_3_2016.append(row,ignore_index=True)
+                elif year == 2015 :
+                    data_3_2015 = data_3_2015.append(row,ignore_index=True)
+
+        print("Completed loading "+str(item)+"!")
+        print("Saving to csv file...")
+        data_1_2018.to_csv(output1,encoding="utf-8",mode="a",header=False)
+        data_1_2017.to_csv(output2,encoding="utf-8",mode="a",header=False)
+        data_1_2016.to_csv(output3,encoding="utf-8",mode="a",header=False)
+        data_1_2015.to_csv(output4,encoding="utf-8",mode="a",header=False)
+
+        data_2_2018.to_csv(output21,encoding="utf-8",mode="a",header=False)
+        data_2_2017.to_csv(output22,encoding="utf-8",mode="a",header=False)
+        data_2_2016.to_csv(output23,encoding="utf-8",mode="a",header=False)
+        data_2_2015.to_csv(output24,encoding="utf-8",mode="a",header=False)
+
+        data_3_2018.to_csv(output31,encoding="utf-8",mode="a",header=False)
+        data_3_2017.to_csv(output32,encoding="utf-8",mode="a",header=False)
+        data_3_2016.to_csv(output33,encoding="utf-8",mode="a",header=False)
+        data_3_2015.to_csv(output34,encoding="utf-8",mode="a",header=False)
+    except Exception as ex:
+        print('can not read file {} {}'.format(item, ex))
+
+
+
+
+print("All done!")
